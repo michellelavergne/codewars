3a43d80138b56c7182163b89e40b8fafbb258bcf
Michelle Lavergne <michellelavergne.dev@gmail.com>
2021-02-23 10:47:21 -0500

    Basic Mathematical Operations

---
+++ codewar13.js
@@ -0,0 +1,16 @@
+// Basic Mathematical Operations
+function basicOp(operation, value1, value2)
+{
+  if (operation === "+") {
+    return value1 + value2
+  
+  }else if (operation === "-") {
+    return value1 - value2  
+  }else if(operation === "*") {
+    return value1 * value2
+  
+  }else if (operation === "/") {
+    return value1 / value2
+  
+  }
+}
