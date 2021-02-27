2bc1f2750823d535cfc2a3a56317c86be47187b7
Michelle Lavergne <michellelavergne.dev@gmail.com>
2021-02-27 10:00:11 -0500

    do I get a bonus?

---
+++ codewar17.js
@@ -0,0 +1,13 @@
+// Do I get a bonus
+
+function bonusTime(salary, bonus) {
+  
+if (bonus ===true) {
+let dollarBills = salary * 10
+return `£${dollarBills}`
+
+} else if (bonus === false) {
+let dollarBills = salary
+return `£${dollarBills}`
+}
+}
