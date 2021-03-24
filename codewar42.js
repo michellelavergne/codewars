61182e0507712801b45d88435c2e2d4b36a5180e
Michelle Lavergne <michellelavergne.dev@gmail.com>
2021-03-24 10:18:31 -0400

    online rpg

---
+++ codewar42.js
@@ -0,0 +1,10 @@
+// Online RPG - player to qualifying stage?
+
+function playerRankUp (points) {
+  if(points >= 100) {
+    return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
+  }else {
+    return false
+  }
+
+}
