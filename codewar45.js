cd96e2dcfb6594075b1f4f599683971d2a827f0d
Michelle Lavergne <michellelavergne.dev@gmail.com>
2021-03-27 10:39:02 -0400

    grader

---
+++ codewar45.js
@@ -0,0 +1,15 @@
+// grader
+function grader(score) {
+  if(score > 1 || score < 0.6) {
+    return "F"
+  }else if (score >= 0.9) {
+    return "A"
+  } else if (score >= 0.8) {
+    return "B"
+  } else if (score >= 0.7) {
+    return "C"
+  } else if (score >= 0.6) {
+    return "D"
+  }
+  
+}
