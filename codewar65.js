7b221d5f1be6b69c7bc450ae06def9a9bc0935c0
Michelle Lavergne <michellelavergne.dev@gmail.com>
2021-04-16 10:47:50 -0400

    Find Maximum and Minimum Values of a List

---
+++ codewar65.js
@@ -0,0 +1,11 @@
+// Find Maximum and Minimum Values of a List
+
+var min = function(list){
+    
+    return Math.min.apply(null,list);
+}
+
+var max = function(list){
+    
+    return Math.max.apply(null,list);
+}
