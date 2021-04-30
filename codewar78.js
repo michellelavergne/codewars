15bea07a3a41bc160318f01e7e946f1ea97a6725
Michelle Lavergne <michellelavergne.dev@gmail.com>
2021-04-30 00:08:48 -0400

    vowel fixed

--- codewar78.js
+++ codewar78.js
@@ -1,13 +1,18 @@
 // Is there a vowel in there?
 
 function isVow(a){
-  
-  for (var i = 0; l = a.length; i < l; ++i)
-    {
-      var char = String.fromCharCode(a[i])
-      if('aeiou'.indexOf(char) !== -1)
-      
-      a[i] = char; 
-    }
-  return a;
+ return a.map(i =>{
+   if (String.fromCharCode(i) === 'a')
+     return 'a';
+   else if (String.fromCharCode(i)=== 'e')
+     return 'e';
+   else if (String.fromCharCode(i)=== 'i')
+     return 'i';
+   else if (String.fromCharCode(i)=== 'o')
+     return 'o';
+   else if (String.fromCharCode(i)=== 'u')
+     return 'u';
+   else
+     return i;
+ })
 }
